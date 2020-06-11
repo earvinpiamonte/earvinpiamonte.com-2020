15ea7a44539679f441d469f9dce37cc3117bbabc
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-06-11 16:45:26 +0800

    Add my Jennie Kim my loves

---
+++ src/pages/404.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+import "../styles/app.css"
+
+import jennie from "../images/jennie-sigh.gif"
+
+export default function PageNotFound() {
+  return (
+    <div className="flex items-center bg-black h-full">
+      <div className="flex-1 text-center">
+        <div className="flex justify-center mb-4">
+          <img
+            src={jennie}
+            className="object-contain h-50"
+            alt="Jennie Kim <3"
+          />
+        </div>
+        <p className="text-2xl text-white">Page not found {":("}</p>
+      </div>
+    </div>
+  )
+}
