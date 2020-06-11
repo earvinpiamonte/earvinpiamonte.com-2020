7d86670da074e42d6502f62cf04ad15aedf7a2b5
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-06-11 17:20:03 +0800

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,8 @@ import "../styles/app.css"
 
 export default function Home() {
   return (
-    <div className="flex items-center bg-gray-200 h-full">
-      <div className="flex-1 text-center">
+    <div className="flex items-center bg-black h-full">
+      <div className="flex-1 text-center text-gray-200">
         <h1 className="text-2xl mb-2">Noel Earvin Piamonte</h1>
         <div className="mb-8 text-lg">
           <p className="mb-1">
@@ -38,7 +38,7 @@ export default function Home() {
             href="https://drive.google.com/open?id=1fyKTOnutzM3Ke5pFpzto0L1FQYeXeVBp"
             target="_blank"
             rel="noopener noreferrer"
-            className="bg-black text-white hover:bg-white hover:text-black font-bold py-3 px-4 rounded"
+            className="bg-gray-100 text-black hover:bg-gray-500 font-bold py-2 px-4 rounded"
           >
             View Resume
           </a>
