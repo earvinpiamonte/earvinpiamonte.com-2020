eab2f5b8b48b58e746fcb7a8cb03ec1264871294
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-06-13 22:42:52 +0800

    Add OutboundLink component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 
+import { OutboundLink } from "gatsby-plugin-google-analytics"
+
 export default function Home() {
   return (
     <section className="bg-black h-full">
@@ -13,51 +15,51 @@ export default function Home() {
               <p className="mb-1 text-gray-200">
                 Hi there, I&apos;m an independent website and React Native app
                 developer at{" "}
-                <a
+                <OutboundLink
                   href="https://www.freelancer.com/u/earvinpiamonte/"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="underline font-bold text-blue-700"
                 >
                   Freelancer.com
-                </a>
+                </OutboundLink>
                 .
               </p>
               <p className="mb-8 text-gray-200">
                 I'm also a Senior <span className="line-through">Web</span>{" "}
                 Developer for{" "}
-                <a
+                <OutboundLink
                   href="https://bentacos.com/"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="underline font-bold text-blue-700"
                 >
                   Bentacos
-                </a>
+                </OutboundLink>
                 .
               </p>
               <p className="text-gray-200">
                 I love writing{" "}
-                <a
+                <OutboundLink
                   href="https://github.com/earvinpiamonte"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="underline font-bold text-blue-700"
                 >
                   code
-                </a>{" "}
+                </OutboundLink>{" "}
                 and Jennie Kim {"<3"}
               </p>
             </div>
             <div className="mb-4">
-              <a
+              <OutboundLink
                 href="https://drive.google.com/open?id=1fyKTOnutzM3Ke5pFpzto0L1FQYeXeVBp"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="bg-gray-100 text-black hover:bg-gray-500 font-bold py-2 px-4 rounded"
               >
                 View Resume
-              </a>
+              </OutboundLink>
             </div>
           </div>
         </div>
