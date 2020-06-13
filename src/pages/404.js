adae65319deb91cf41bd0b6615b8d249fa198a37
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-06-13 23:59:19 +0800

    Integrate SEO

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,28 +2,34 @@ import React from "react"
 
 import { Link } from "gatsby"
 
+import SEO from "../components/SEO"
+
 import "../styles/app.css"
+import jennieGIF from "../images/jennie-sigh.gif"
 
-import jennie from "../images/jennie-sigh.gif"
-
-export default function PageNotFound() {
+const PageNotFound = () => {
   return (
-    <div className="flex items-center bg-black h-full">
-      <div className="flex-1 text-center">
-        <div className="flex justify-center mb-4">
-          <img
-            src={jennie}
-            className="object-contain h-50"
-            alt="Jennie Kim my loves <3"
-          />
+    <>
+      <SEO title={`Page not found`} />
+      <div className="flex items-center bg-black h-full">
+        <div className="flex-1 text-center">
+          <div className="flex justify-center mb-4">
+            <img
+              src={jennieGIF}
+              className="object-contain h-50"
+              alt="Jennie Kim my loves <3"
+            />
+          </div>
+          <p className="text-2xl text-white mb-4">Page not found {":("}</p>
+          <p className="text-gray-500">
+            <Link to="/" className="font-weight-bold underline">
+              <small>Go to home page</small>
+            </Link>
+          </p>
         </div>
-        <p className="text-2xl text-white mb-4">Page not found {":("}</p>
-        <p className="text-gray-500">
-          <Link to="/" className="font-weight-bold underline">
-            <small>Go to home page</small>
-          </Link>
-        </p>
       </div>
-    </div>
+    </>
   )
 }
+
+export default PageNotFound
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,68 +2,84 @@ import React from "react"
 
 import { OutboundLink } from "gatsby-plugin-google-analytics"
 
-export default function Home() {
+import SEO from "../components/SEO"
+
+import socialPreview from "../images/social-preview.jpg"
+
+const Home = () => {
   return (
-    <section className="bg-black h-full">
-      <div className="container mx-auto px-4 h-full">
-        <div className="grid grid-cols-12 gap-4 content-center h-full">
-          <div className="col-span-8 col-start-3">
-            <h1 className="text-3xl mb-4 text-gray-200 tracking-wider uppercase">
-              Noel Earvin Piamonte &middot; Software Developer
-            </h1>
-            <div className="mb-10 text-lg tracking-wider">
-              <p className="mb-1 text-gray-200">
-                Hi there, I&apos;m an independent website and React Native app
-                developer at{" "}
+    <>
+      <SEO
+        title={`Software Developer`}
+        image={{
+          src: socialPreview,
+          width: 1280,
+          height: 640,
+        }}
+      />
+      <section className="bg-black h-full">
+        <div className="container mx-auto px-4 h-full">
+          <div className="grid grid-cols-12 gap-4 content-center h-full">
+            <div className="col-span-8 col-start-3">
+              <h1 className="text-3xl mb-4 text-gray-200 tracking-wider uppercase">
+                Noel Earvin Piamonte &middot; Software Developer
+              </h1>
+              <div className="mb-10 text-lg tracking-wider">
+                <p className="mb-1 text-gray-200">
+                  Hi there, I&apos;m an independent website and React Native app
+                  developer at{" "}
+                  <OutboundLink
+                    href="https://www.freelancer.com/u/earvinpiamonte/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="underline font-bold text-blue-700"
+                  >
+                    Freelancer.com
+                  </OutboundLink>
+                  .
+                </p>
+                <p className="mb-8 text-gray-200">
+                  I'm also a Senior <span className="line-through">Web</span>{" "}
+                  Developer for{" "}
+                  <OutboundLink
+                    href="https://bentacos.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="underline font-bold text-blue-700"
+                  >
+                    Bentacos
+                  </OutboundLink>
+                  .
+                </p>
+                <p className="text-gray-200">
+                  I love writing{" "}
+                  <OutboundLink
+                    href="https://github.com/earvinpiamonte"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="underline font-bold text-blue-700"
+                  >
+                    code
+                  </OutboundLink>{" "}
+                  and Jennie Kim {"<3"}
+                </p>
+              </div>
+              <div className="mb-4">
                 <OutboundLink
-                  href="https://www.freelancer.com/u/earvinpiamonte/"
+                  href="https://drive.google.com/open?id=1fyKTOnutzM3Ke5pFpzto0L1FQYeXeVBp"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="underline font-bold text-blue-700"
+                  className="bg-gray-100 text-black hover:bg-gray-500 font-bold py-2 px-4 rounded"
                 >
-                  Freelancer.com
+                  View Resume
                 </OutboundLink>
-                .
-              </p>
-              <p className="mb-8 text-gray-200">
-                I'm also a Senior <span className="line-through">Web</span>{" "}
-                Developer for{" "}
-                <OutboundLink
-                  href="https://bentacos.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="underline font-bold text-blue-700"
-                >
-                  Bentacos
-                </OutboundLink>
-                .
-              </p>
-              <p className="text-gray-200">
-                I love writing{" "}
-                <OutboundLink
-                  href="https://github.com/earvinpiamonte"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="underline font-bold text-blue-700"
-                >
-                  code
-                </OutboundLink>{" "}
-                and Jennie Kim {"<3"}
-              </p>
-            </div>
-            <div className="mb-4">
-              <OutboundLink
-                href="https://drive.google.com/open?id=1fyKTOnutzM3Ke5pFpzto0L1FQYeXeVBp"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="bg-gray-100 text-black hover:bg-gray-500 font-bold py-2 px-4 rounded"
-              >
-                View Resume
-              </OutboundLink>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
+
+export default Home
