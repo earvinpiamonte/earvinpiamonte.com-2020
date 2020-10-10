0a1b6b6484160fd3db7b6a56570662143948f4bc
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-10-11 02:32:30 +0800

    Import projects as component

---
+++ src/components/projects.js
@@ -0,0 +1,105 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+
+import { OutboundLink } from "gatsby-plugin-google-analytics"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+
+const ProjectsQuery = graphql`
+  {
+    projects: allMdx(filter: { frontmatter: { type: { eq: "project" } } }) {
+      edges {
+        node {
+          id
+          excerpt
+          body
+          frontmatter {
+            slug
+            type
+            title
+            url
+            year
+            categories
+            featuredImage {
+              publicURL
+            }
+            thumbnail {
+              publicURL
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const Projects = () => {
+
+  return (
+    <StaticQuery
+      query={ProjectsQuery}
+      render={data => data.projects.edges.map(({ node }) => {
+        const {
+          title,
+          slug,
+          url,
+          year,
+          categories,
+          thumbnail
+        } = node.frontmatter
+
+        const body = node.body
+
+        return (
+          <article className="mb-12" key={node.id}>
+            <div className="grid grid-cols-12 gap-4">
+              <div className="lg:col-span-5 col-span-12">
+                <img
+                    src={thumbnail.publicURL}
+                    alt={`${title} web page screenshot,`}
+                    className="border-2 border-blue-700 rounded-lg"
+                    draggable={false}
+                  />
+              </div>
+              <div className="lg:col-span-7 col-span-12">
+                <h2
+                  className="mb-1 uppercase font-bold"
+                  data-slug={slug}
+                >
+                  {title}
+                </h2>
+                <p className="mb-3 text-gray-600 text-sm">
+                  {year} {" "}
+                  &mdash; {" "}
+                  {categories.map((category, key) => (
+                    <span key={key}>
+                      {`${category}`}
+                      {key === categories.length - 1 ? " " : ", "}
+                    </span>
+                  ))}
+                </p>
+
+                <div className="mb-3">
+                  <MDXRenderer>{body}</MDXRenderer>
+                </div>
+
+                <p className="mb-3">
+                  <OutboundLink
+                    href={url}
+                    className="text-blue-500 underline font-bold"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    View site
+                  </OutboundLink>
+                </p>
+
+              </div>
+            </div>
+          </article>
+        )
+      })}
+    />
+  )
+}
+
+export default Projects
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,109 +1,14 @@
 import React from "react"
-import { graphql } from "gatsby"
 
 import { OutboundLink } from "gatsby-plugin-google-analytics"
-import { MDXRenderer } from "gatsby-plugin-mdx"
 
 import SEO from "../components/seo"
 import Header from "../components/header"
 
-import "../styles/app.css"
+import Projects from '../components/projects'
 
-export const pageQuery = graphql`
-  {
-    projects: allMdx(filter: { frontmatter: { type: { eq: "project" } } }) {
-      edges {
-        node {
-          id
-          excerpt
-          body
-          frontmatter {
-            slug
-            type
-            title
-            url
-            year
-            categories
-            featuredImage {
-              publicURL
-            }
-            thumbnail {
-              publicURL
-            }
-          }
-        }
-      }
-    }
-  }
-`
 
-const Portfolio = ({ data }) => {
-  const { edges: projects } = data.projects
-
-  const Projects = () => {
-    return projects.map(({ node }) => {
-      const {
-        title,
-        slug,
-        url,
-        year,
-        categories,
-        featuredImage,
-        thumbnail
-      } = node.frontmatter
-
-      const body = node.body
-
-      return (
-        <article className="mb-12" key={node.id}>
-          <div className="grid grid-cols-12 gap-4">
-            <div className="lg:col-span-5 col-span-12">
-              <img
-                  src={thumbnail.publicURL}
-                  alt={`${title} web page screenshot,`}
-                  className="border-2 border-blue-700 rounded-lg"
-                  draggable={false}
-                />
-            </div>
-            <div className="lg:col-span-7 col-span-12">
-              <h2
-                className="mb-1 uppercase font-bold"
-                data-slug={slug}
-              >
-                {title}
-              </h2>
-              <p className="mb-3 text-gray-600 text-sm">
-                {year} {" "}
-                &mdash; {" "}
-                {categories.map((category, key) => (
-                  <span key={key}>
-                    {`${category}`}
-                    {key === categories.length - 1 ? " " : ", "}
-                  </span>
-                ))}
-              </p>
-
-              <div className="mb-3">
-                <MDXRenderer>{body}</MDXRenderer>
-              </div>
-
-              <p className="mb-3">
-                <OutboundLink
-                  href={url}
-                  className="text-blue-500 underline font-bold"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View site
-                </OutboundLink>
-              </p>
-
-            </div>
-          </div>
-        </article>
-      )
-    })
-  }
+const Portfolio = () => {
 
   return (
     <>
@@ -112,11 +17,11 @@ const Portfolio = ({ data }) => {
       <main className="container mx-auto">
         <div className="grid grid-cols-12 gap-4">
           <div className="col-span-12 md:col-span-8 md:col-start-3 px-6">
-            <h1 className="text-4xl text-gray-900 mb-10">Portfolio</h1>
+            <h1 className="text-4xl mb-10">Portfolio</h1>
 
             <div className="mb-8">
               <p>
-                You'll find here are some of the previous projects I worked on as an independent developer. You may also check my {" "}
+                You'll find here are some of the previous projects I worked on as an independent developer. Visit my {" "}
                 <OutboundLink
                   href="https://github.com/earvinpiamonte"
                   target="_blank"
@@ -129,7 +34,7 @@ const Portfolio = ({ data }) => {
               </p>
             </div>
 
-            <Projects className="mb-16" />
+            <Projects />
 
             <p className="text-sm mt-16 text-gray-500">
               <abbr title="야!">Ya!</abbr> this page is still in the works.
