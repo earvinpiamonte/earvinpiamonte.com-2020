68cbdefbae13175af2b9f57aba589863d1be1bb7
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-10-11 11:29:38 +0800

    Update `Playlist` page

--- src/pages/playlist.js
+++ src/pages/playlist.js
@@ -1,9 +1,9 @@
 import React from "react"
 
-import { OutboundLink } from "gatsby-plugin-google-analytics"
-
 import SEO from "../components/seo"
 import Header from "../components/header"
+
+import socialPreview from "../images/playlist-social-preview.jpg"
 
 
 const Playlist = () => {
@@ -11,6 +11,11 @@ const Playlist = () => {
     <>
       <SEO
         title={`Check out the songs that I'm recently listening to`}
+        image={{
+          src: socialPreview,
+          width: 1280,
+          height: 640,
+        }}
       />
       <Header />
       <main className="container mx-auto">
@@ -23,8 +28,8 @@ const Playlist = () => {
               </p>
             </article>
 
-            <div className="mb-4 responsive-embed">
-              <iframe src="https://open.spotify.com/embed/playlist/6SICjj0WVSCCE0phlvyR89" width="640" height="720" class="mx-auto" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            <div className="mb-64 responsive-embed border-2 bg-gray-200 border-gray-900 rounded-lg">
+              <iframe src="https://open.spotify.com/embed/playlist/6SICjj0WVSCCE0phlvyR89" title="Spotify playlist" width="640" height="720" class="mx-auto" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
             </div>
 
           </div>
