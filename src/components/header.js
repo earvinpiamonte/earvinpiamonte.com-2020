93882ddd2932c2b4f2c0dd8f754765db109838ee
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-10-17 17:01:19 +0800

    Update svg `stroke-*` attributes

--- src/components/header.js
+++ src/components/header.js
@@ -27,7 +27,7 @@ const Header = props => {
                     onClick={() => setDropdownMenuOpen(!dropdownMenuOpen)}
                   >
                     <svg
-                      class="w-6 h-6"
+                      className="w-6 h-6"
                       fill="none"
                       stroke="currentColor"
                       viewBox="0 0 24 24"
@@ -35,17 +35,17 @@ const Header = props => {
                     >
                       {!dropdownMenuOpen && (
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="M4 6h16M4 12h16M4 18h16"
                         ></path>
                       )}
                       {dropdownMenuOpen && (
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="M6 18L18 6M6 6l12 12"
                         ></path>
                       )}
@@ -112,9 +112,9 @@ const Header = props => {
                       xmlns="http://www.w3.org/2000/svg"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                         d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
                       ></path>
                     </svg>
