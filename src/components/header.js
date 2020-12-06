f690ed1278c7c97a5826650d16af1902b50ac150
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-12-06 16:45:34 +0800

    Update navigation

--- src/components/header.js
+++ src/components/header.js
@@ -115,7 +115,7 @@ const Header = props => {
                         d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
                       ></path>
                     </svg>
-                    <span className="ml-1 align-middle">Hire me</span>
+                    <span className="ml-1 align-middle">Freelancer.com</span>
                   </OutboundLink>
                 </div>
               </div>
