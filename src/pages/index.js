3282e800ce8ce10fd1a8651b69d1c981b66ca46a
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-12-06 16:45:45 +0800

    Update home page text copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,20 +24,26 @@ const Home = () => {
         <div className="container mx-auto h-full">
           <div className="grid grid-cols-12 gap-4 content-center h-full">
             <div className="col-span-12 md:col-span-8 md:col-start-3 px-6">
-              <h1 className="text-3xl mb-4 text-gray-200 tracking-wider uppercase">
-                Noel Earvin Piamonte{" "}
-                <span className="line-through mx-4">&#47;</span> Software
-                Developer
+              <h1 className="text-4xl mb-4 text-gray-200">
+                Hello there, I'm Noel Earvin.
               </h1>
               <div className="mb-10 text-lg tracking-wider">
                 <p className="mb-1 text-gray-200 mb-8">
-                  Hi there. I&apos;m an independent software developer focused
-                  on web and React Native app development &mdash; Previously,
-                  senior web developer for Bentacos in San Juan City,{" "}
-                  <abbr title="Philippines">PH</abbr>.
+                  I'm a Software Engineer at{" "}
+                  <OutboundLink
+                    href="https://www.infor.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="underline font-bold text-blue-700"
+                  >
+                    Infor PSSC, Inc.
+                  </OutboundLink>{" "}
+                  in Taguig City, <abbr title="Philippines">PH</abbr>.
+                  Previously, I was an Independent Software Developer focused on
+                  web and React Native app development.
                 </p>
                 <p className="text-gray-200">
-                  I love writing{" "}
+                  Outside work, I write{" "}
                   <OutboundLink
                     href="https://github.com/earvinpiamonte"
                     target="_blank"
@@ -46,10 +52,7 @@ const Home = () => {
                   >
                     code
                   </OutboundLink>{" "}
-                  and Jennie Kim{" "}
-                  <span title="공주님" className="text-white">
-                    {"<3"}
-                  </span>
+                  for the web.
                 </p>
               </div>
             </div>
