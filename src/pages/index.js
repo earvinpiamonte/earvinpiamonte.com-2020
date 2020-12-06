51a1cb86994c7fc65c0bf789ff3320de8cf2f771
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-12-07 01:08:03 +0800

    Update home page text copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const Home = () => {
   return (
     <>
       <SEO
-        title={`Software Developer`}
+        title={`Software Engineer`}
         image={{
           src: socialPreview,
           width: 1280,
@@ -24,8 +24,8 @@ const Home = () => {
         <div className="container mx-auto h-full">
           <div className="grid grid-cols-12 gap-4 content-center h-full">
             <div className="col-span-12 md:col-span-8 md:col-start-3 px-6">
-              <h1 className="text-4xl mb-4 text-gray-200">
-                Hello there, I'm Noel Earvin.
+              <h1 className="text-4xl mb-4 text-gray-200 tracking-wider">
+                Hello there. I'm Noel Earvin.
               </h1>
               <div className="mb-10 text-lg tracking-wider">
                 <p className="mb-1 text-gray-200 mb-8">
@@ -39,20 +39,20 @@ const Home = () => {
                     Infor PSSC, Inc.
                   </OutboundLink>{" "}
                   in Taguig City, <abbr title="Philippines">PH</abbr>.
-                  Previously, I was an Independent Software Developer focused on
-                  web and React Native app development.
+                  Previously, an Independent Software Developer focused on web
+                  and React Native app development.
                 </p>
                 <p className="text-gray-200">
-                  Outside work, I write{" "}
+                  Outside work, I enjoy{" "}
                   <OutboundLink
                     href="https://github.com/earvinpiamonte"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="underline font-bold text-blue-700"
                   >
-                    code
+                    writing
                   </OutboundLink>{" "}
-                  for the web.
+                  projects for the web.
                 </p>
               </div>
             </div>
